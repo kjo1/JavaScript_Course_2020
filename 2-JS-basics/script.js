c9952aa5dc99453f8183e324092a8ef23dd97b67
Kanghyun Jo <kjo0@outlook.com>
2020-01-29 18:13:52 -0500

    Variable Mutation and Type Coercion - Day 28

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,3 +1,9 @@
+/********************
+ * Variables and data types
+ *   
+ */
+
+/*
 // string
 var firstName = 'John';
 console.log(firstName);
@@ -17,6 +23,40 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'Hohn and Mark';
+var if = 23; // can't be used
+
 // variable names work with string, $ and _
 // better startin with a letter, $ and _
 // cannot use reserved JS words like delete, function and if
+*/
+
+/********************
+ * Variables mutation and type coercion
+ *   
+ */
+
+var firstName = 'John'
+var age = 21;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old '
+    + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty one';
+job = 'drive';
+
+alert(firstName + ' is a ' + age + ' year old '
+    + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
