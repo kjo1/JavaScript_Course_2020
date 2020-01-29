79aba9c81b1d3f18668f1c372c40c4d66b58ad2c
Kanghyun Jo <kjo0@outlook.com>
2020-01-29 17:36:26 -0500

    Variables and Data Types - Day 28

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,1 +1,22 @@
-console.log('Hello World!!!');
+// string
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+// number
+var age = 28;
+
+// boolean
+var fullAge = true;
+console.log(fullAge);
+
+// undefined
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// variable names work with string, $ and _
+// better startin with a letter, $ and _
+// cannot use reserved JS words like delete, function and if
