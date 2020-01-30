c150d2ab580d4617aeb9cf7a70dc452a6f0f5e31
Kanghyun Jo <kjo0@outlook.com>
2020-01-30 10:56:28 -0500

    Basic precedence - Day 29

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -67,6 +67,7 @@ console.log(firstName + ' ' + lastName);
 /************************************
  * Basic operators
  */
+/*
 var year, yearJohn, yearMark;
 now = 2020;
 ageJohn = 29;
@@ -96,3 +97,39 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
 console.log(typeof x);
+*/
+
+/************************************
+ * Basic precedence
+ */
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Mutiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x);
+console.log(y);
+
+// More operators
+x *= 2; //x = x * 2;
+console.log(x);
+x += 10;
+console.log(x);
+
+x++; //x = x + 1; //x += 1;
+console.log(x);
+x--;
+console.log(x);
