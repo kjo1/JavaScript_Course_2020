ca350210d41965613011498516d2298294fc8a33
Kanghyun Jo <kjo0@outlook.com>
2020-02-08 10:01:26 -0500

    First Class Functions: Passing Functions as Arguments - Day 38

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -55,7 +55,7 @@ var jane = Object.create(personProto,
 */
 
 // Primitives vs Objects
-
+/*
 // Primitives
 var a = 23;
 var b = a;
@@ -89,10 +89,45 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
+*/
 
 
+// First Class Functions: Passing Functions as Arguments
 
+var years = [1990, 1965, 1937, 2005, 1998];
 
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2020 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    }
+    else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
 
 
 
