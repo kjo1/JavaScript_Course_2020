8bfe50507a639900650b4c86219426cee37db29f
Kanghyun Jo <kjo0@outlook.com>
2020-02-13 17:39:26 -0500

    CODING CHALLENGE 7: SOLUTION_Expert Level - Day 43

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -310,7 +310,8 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
-
+// Basic Level
+/*
 (function () {
     function Question(question, answers, correct) {
         this.question = question;
@@ -354,5 +355,80 @@ c) correct answer (I would use a number for this)
 
     questions[n].checkAnswer(answer);
 })();
+*/
 
+// Expert Level
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
 
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function (ans, callback) {
+        var sc;
+
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+            sc = callback(true);
+        }
+        else {
+            console.log('Wrong answer. Try again :');
+
+            sc = callback(false);
+        }
+        this.dsiplayScore(sc);
+    }
+
+    Question.prototype.dsiplayScore = function (score) {
+        console.log('Your current score is: ' + score);
+        console.log('-----------------------------------');
+    }
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+        ['Yes', 'No'], 0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?',
+        ['John', 'Micheal', 'Jonas'], 2);
+
+    var q3 = new Question('What does best descibe coding?',
+        ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+    var questions = [q1, q2, q3];
+
+    function score() {
+        var sc = 0;
+        return function (correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+
+    var keepScore = score();
+
+    function nextQuestion() {
+
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer.');
+
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+
+            nextQuestion();
+        }
+    }
+    nextQuestion();
+})();
